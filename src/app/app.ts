671315d9104bd16aaa9a965e13b49ee831636d3c
HooNDa-003 <90273159+mohand003@users.noreply.github.com>
2025-07-27 23:39:25 +0300

    fix: remove unused HomeComponent import from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { HomeComponent } from './pages/home/home';
 import { HeaderComponent } from './components/header/header';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
